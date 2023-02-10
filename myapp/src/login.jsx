72e6fe47e85e5d01abedee7ce51d77fa82c9ed2c
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-02-10 22:56:53 +0530

    fix the footer in Login.Jsx

--- myapp/src/login.jsx
+++ myapp/src/login.jsx
@@ -8,65 +8,106 @@ import {
   Card,
   HStack,
   Button,
+  Stack,
+  CardBody,
+  Link,
+  Text,
+  Center,
 } from "@chakra-ui/react";
 import { BRANDLOGO } from "./signIn-logo";
 
 export function Login() {
   return (
     <Box>
-      <VStack as="header" spacing="6" mt="8">
-        <BRANDLOGO />
-        <Heading
-          as="h1"
-          fontWeight="300"
-          fontSize="24px"
-          letterSpacing="-0.5px"
-        >
-          Sign in to Zing
-        </Heading>
-      </VStack>
-      <Card bg="#FFFFF0 " variant="outline" borderColor="#4299E1 " maxW="380px">
-        <FormControl>
-          <FormLabel size="sm">UserName or Email address </FormLabel>
-          <Input
-            type="text"
-            bg="Orange 50
-"
-            borderColor="#E6FFFA
-"
-            size="sm"
-            borderRadius="10px"
-          />
-        </FormControl>
-        <FormControl>
-          <HStack>
-            <FormLabel size="sm">UserName or Email address </FormLabel>
-            <Button
-              as="a"
-              href="#"
-              variant="link"
-              size="xs"
-              color="#3182CE"
-              fontWeight="700"
+      <Center>
+        <Stack spacing="6">
+          <VStack as="header" spacing="6" mt="16">
+            <BRANDLOGO />
+            <Heading
+              as="h1"
+              fontWeight="300"
+              fontSize="24px"
+              letterSpacing="-0.5px"
             >
-              {" "}
-              Forget Password?
-            </Button>
-          </HStack>
-          <Input
-            type="text"
-            bg="Orange 50
+              Sign in to Zing
+            </Heading>
+          </VStack>
+
+          <Card
+            bg="#FFFFF0 "
+            variant="outline"
+            borderColor="#4299E1 "
+            maxW="380px"
+          >
+            <CardBody>
+              <form>
+                <Stack spacing="2">
+                  <FormControl>
+                    <FormLabel size="sm">UserName or Email address </FormLabel>
+                    <Input
+                      type="text"
+                      bg="Orange 50
 "
-            borderColor="#E6FFFA
+                      borderColor="#E6FFFA
 "
-            size="sm"
-            borderRadius="10px"
-          />
-        </FormControl>
-        <Button bg="#2EA033  " color="white" size="sm" _hover="">
-          Sign In
-        </Button>
-      </Card>
+                      size="sm"
+                      borderRadius="10px"
+                    />
+                  </FormControl>
+
+                  <FormControl>
+                    <HStack>
+                      <FormLabel size="sm">Password</FormLabel>
+                      <Button
+                        as="a"
+                        href="#"
+                        variant="link"
+                        size="xs"
+                        color="#3182CE"
+                        fontWeight="700"
+                      >
+                        {" "}
+                        Forget Password?
+                      </Button>
+                    </HStack>
+                    <Input
+                      type="text"
+                      bg="Orange 50
+"
+                      borderColor="#E6FFFA
+"
+                      size="sm"
+                      borderRadius="10px"
+                    />
+                  </FormControl>
+                  <Button
+                    bg="#2EA033  "
+                    color="white"
+                    size="sm"
+                    _hover={{ bg: "#29B82F" }}
+                    _active={{ bg: "#2EA033" }}
+                  >
+                    Sign In
+                  </Button>
+                </Stack>
+              </form>
+            </CardBody>
+          </Card>
+          <Card variant="outline " borderColor=" #2C5EEC" borderRadius={"md"}>
+            <CardBody>
+              <Center>
+                <HStack spacing="4" fontSize={"sm"}>
+                  <Text>New account to Join?</Text>
+                  <Link isExternal color="#2C5EEC" href="#">
+                    Create an account
+                  </Link>
+                </HStack>
+              </Center>
+            </CardBody>
+          </Card>
+        </Stack>
+      </Center>
+      <HStack></HStack>
     </Box>
   );
 }
